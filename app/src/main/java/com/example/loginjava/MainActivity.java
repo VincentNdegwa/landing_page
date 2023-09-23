91ba5a5c567cfcc637ab4b09cf46939c2b1e362a
VincentNdegwa <ndegwavincent7@gmail.com>
2023-09-24 01:10:56 +0300

    prepared the interface

--- app/src/main/java/com/example/loginjava/MainActivity.java
+++ app/src/main/java/com/example/loginjava/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.loginjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                navigateToLogin();
+            }
+        },3000);
+    }
+
+    private void navigateToLogin() {
+        Intent userintent = new Intent(this, UserPage.class);
+        startActivity(userintent);
     }
 }
